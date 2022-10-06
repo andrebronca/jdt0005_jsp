78ceda482ed1a59aefd93dfdedfb55f3095fd900
André R. B <bronca.andre@gmail.com>
2022-10-05 22:51:58 -0300

    configurando simulador de login com redirecionamento. obs. não está funcionando o simulador

--- src/main/java/com/arb/servlets/ServletLogin.java
+++ src/main/java/com/arb/servlets/ServletLogin.java
@@ -15,36 +15,57 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-@WebServlet("/ServletLogin")	
+@WebServlet("/ServletLogin")
 public class ServletLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    public ServletLogin() {
-    }
 
-    /*
-     * recebe os dados pela url
-     */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletLogin() {
+	}
+
+	/*
+	 * recebe os dados pela url
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 	}
 
 	/*
 	 * recebe os dados de formul√°rio
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ModelLogin mLogin;
 		String login = request.getParameter("login");
 		String senha = request.getParameter("senha");
-		if (login != null && !login.isEmpty() && senha != null && !senha.isEmpty()) {
-			ModelLogin mLogin = new ModelLogin();
+		if ((login != null && !login.isEmpty()) && (senha != null && !senha.isEmpty())) {
+			mLogin = new ModelLogin();
 			mLogin.setLogin(login);
 			mLogin.setSenha(senha);
+			
+			//simulando um login com direcionamento para p·gina administrativa
+			if(mLogin.getLogin().equalsIgnoreCase("admin") && mLogin.getSenha().equalsIgnoreCase("admin")) {
+				request.getSession().setAttribute("usuario", mLogin.getLogin());
+				RequestDispatcher redirecionar = request.getRequestDispatcher("principal/principal.jsp");
+				redirecionar.forward(request, response);
+			} else {
+				RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
+				request.setAttribute("msg", "Informe o login e senha corretamente!");
+				redirecionar.forward(request, response);		
+			}
 		} else {
 			RequestDispatcher redirecionar = request.getRequestDispatcher("index.jsp");
 			request.setAttribute("msg", "Informe o login e senha corretamente!");
 			redirecionar.forward(request, response);
 		}
-		
+
 	}
 
+	private void redirectComMsg(String toPage, String msg, HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		RequestDispatcher redirecionar = request.getRequestDispatcher(toPage);
+		if(msg != null) {
+			request.setAttribute("msg", msg);
+		}
+		redirecionar.forward(request, response);
+	}
 }
